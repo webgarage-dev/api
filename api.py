025970e56d4f44a669e2ea3f9801121e7824dbe0
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-09-24 22:02:25 +0300

    first API function with Postman assists

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+
+@app.route("/get-quote", methods=["POST"])
+def get_quote():
+    api_language_selector = request.args.get("language_selector", "en")
+    api_url = f"https://api.forismatic.com/api/1.0/?method=getQuote&lang={api_language_selector}&format=json"
+    return requests.get(api_url).content
