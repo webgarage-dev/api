0693c1730dbe789aa217184ea092bba03eab6433
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-05 19:44:17 +0300

    add CORS header only in debug mode

--- wg_api.py
+++ wg_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response
 import requests
+import os
 app = Flask(__name__)
 
 
@@ -12,13 +13,17 @@ def index():
 
 @app.route("/quotes/get/<lang>")
 def getquote(lang):
+    mode = os.environ.get("MODE")
     api_url = f"https://api.forismatic.com/api/1.0/?method=getQuote&lang={lang}&format=json"
     quote_json = requests.get(api_url).content
     result = make_response(quote_json)
     result.headers['Content-Type'] = 'text/json'
-    result.headers['Access-Control-Allow-Origin'] = '*'
+    if mode == "debug":
+        result.headers['Access-Control-Allow-Origin'] = '*'
     return result
 
+
+# -------------------------- RUN --------------------------
 
 if __name__ == "__main__":
     app.run()
