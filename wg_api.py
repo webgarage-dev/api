5d64771468cd50a073e7e87bbff1e759a7cf48ae
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-06 22:21:04 +0300

    setting up "push-to-deploy"

--- wg_api.py
+++ wg_api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "API description"
+    return "API description here"
 
 
 # -------------------------- project QUOTES --------------------------
