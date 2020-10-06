5d3dd14f84fe49fca860b56f8d98f71e827ecfa6
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-06 09:08:48 +0300

    minify response

--- wg_api.py
+++ wg_api.py
@@ -1,5 +1,6 @@
-from flask import Flask, make_response
 import requests
+import json
+from flask import Flask, make_response, jsonify
 from settings import WG_API_MODE, WG_CORS_DOMAIN
 app = Flask(__name__)
 
@@ -15,8 +16,12 @@ def index():
 @app.route("/quotes/get/<lang>")
 def getquote(lang):
     api_url = f"https://api.forismatic.com/api/1.0/?method=getQuote&lang={lang}&format=json"
-    quote_json = requests.get(api_url).content
-    result = make_response(quote_json)
+    quote_dict = json.loads(requests.get(api_url).content)
+    result_dict = {
+        "quoteText": quote_dict.get("quoteText"),
+        "quoteAuthor": quote_dict.get("quoteAuthor")
+    }
+    result = make_response(jsonify(result_dict))
     result.headers['Content-Type'] = 'text/json'
     if WG_API_MODE == "debug":
         result.headers['Access-Control-Allow-Origin'] = "*"
