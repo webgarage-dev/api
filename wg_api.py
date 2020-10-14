0f4db29dae08a01da1f8ae31e3dde45b8180ea0b
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-14 21:25:01 +0300

    get different amount of photos

--- wg_api.py
+++ wg_api.py
@@ -32,10 +32,13 @@ def getquote(lang):
 
 # -------------------------- project UNSPLASH QUOTES --------------------------
 
-@app.route("/uninfscroll/photos/get")
-def get_photos():
-    count = 30
-    api_url = f"https://api.unsplash.com/photos/random/?client_id={UNSPLASH_ACCESS_KEY}&count={count}"
+@app.route("/uninfscroll/photos/get/<int:photo_count>")
+def get_photos(photo_count):
+    if photo_count <= 0:
+        photo_count = 1
+    elif photo_count > 30:
+        photo_count = 30
+    api_url = f"https://api.unsplash.com/photos/random/?client_id={UNSPLASH_ACCESS_KEY}&count={photo_count}"
     print(api_url)
     result = make_response(requests.get(api_url).content)
     if WG_API_MODE == "debug":
