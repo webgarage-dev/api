426f022fb08d18b154b60017131c30b61ddd8934
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-15 23:10:05 +0300

    minor refactoring

--- wg_api.py
+++ wg_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from flask import Flask, make_response, jsonify
-from settings import WG_API_MODE, WG_CORS_DOMAIN, UNSPLASH_ACCESS_KEY
+from settings import UNSPLASH_ACCESS_KEY
 from utils import cors_header_value
 app = Flask(__name__)
 
@@ -37,12 +37,9 @@ def get_photos(photo_count):
     elif photo_count > 30:
         photo_count = 30
     api_url = f"https://api.unsplash.com/photos/random/?client_id={UNSPLASH_ACCESS_KEY}&count={photo_count}"
-    print(api_url)
     result = make_response(requests.get(api_url).content)
-    if WG_API_MODE == "debug":
-        result.headers['Access-Control-Allow-Origin'] = "*"
-    else:
-        result.headers['Access-Control-Allow-Origin'] = WG_CORS_DOMAIN
+    result.headers['Content-Type'] = 'text/json'
+    result.headers["Access-Control-Allow-Origin"] = cors_header_value()
     return result
 
 # -------------------------- RUN --------------------------
